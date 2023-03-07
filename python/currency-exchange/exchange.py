72735d3124b92c2e20c0160d550120f8316f927c
mirhex-toldex <miranda.nichols@gu.se>
2023-03-07 17:48:46 +0100

    currency-exchange: complete

--- python/currency-exchange/exchange.py
+++ python/currency-exchange/exchange.py
@@ -6,7 +6,7 @@ def exchange_money(budget, exchange_rate):
     :return: float - exchanged value of the foreign currency you can receive.
     """
 
-    pass
+    return budget / exchange_rate
 
 
 def get_change(budget, exchanging_value):
@@ -17,7 +17,7 @@ def get_change(budget, exchanging_value):
     :return: float - amount left of your starting currency after exchanging.
     """
 
-    pass
+    return budget - exchanging_value
 
 
 def get_value_of_bills(denomination, number_of_bills):
@@ -28,7 +28,7 @@ def get_value_of_bills(denomination, number_of_bills):
     :return: int - total value of bills you now have.
     """
 
-    pass
+    return denomination * number_of_bills
 
 
 def get_number_of_bills(budget, denomination):
@@ -39,7 +39,7 @@ def get_number_of_bills(budget, denomination):
     :return: int - number of bills after exchanging all your money.
     """
 
-    pass
+    return int(budget / denomination)
 
 
 def get_leftover_of_bills(budget, denomination):
@@ -49,8 +49,8 @@ def get_leftover_of_bills(budget, denomination):
     :param denomination: int - the value of a single bill.
     :return: float - the leftover amount that cannot be exchanged given the current denomination.
     """
-
-    pass
+    
+    return budget % denomination
 
 
 def exchangeable_value(budget, exchange_rate, spread, denomination):
@@ -62,5 +62,7 @@ def exchangeable_value(budget, exchange_rate, spread, denomination):
     :param denomination: int - the value of a single bill.
     :return: int - maximum value you can get.
     """
-
-    pass
+    actual_exchange_rate = (exchange_rate * (spread / 100)) + exchange_rate
+    exchanged_value = exchange_money(budget, actual_exchange_rate) 
+    number_of_bills = get_number_of_bills(exchanged_value, denomination)
+    return get_value_of_bills(denomination, number_of_bills)
